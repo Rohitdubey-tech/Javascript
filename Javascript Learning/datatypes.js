88406c818f886c092f9c5fc888ffff9c867e0a8b
Rohitdubey-tech <rohitdubey0601@gmail.com>
2025-09-28 16:18:50 +0530

    dataTypes

---
+++ Javascript Learning/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer vesrsion
+
+// number => 2 to pow 53
+// bigint => for larger values
+// string => prefer to use in double quotes rather than single quotes
+// boolean => true/false
+// null => stand alone value
+// undefined =>
+// symbol => unique
+// object
+
+
+// let score = 33;
+// console.log(typeof score);
+// let valueInString = String(score);
+// console.log(typeof valueInString);
+
+
+
+// let str1 = "Hello";
+// let str2 = "Rohit";
+// let str3 = str1 + " " +str2;
+// console.log(str3);
