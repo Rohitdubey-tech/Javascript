686e30ef2471dc858c7672632ffa94679a0396a2
Rohitdubey-tech <rohitdubey0601@gmail.com>
2025-09-28 15:29:39 +0530

    variables

---
+++ Javascript Learning/variable.js
@@ -0,0 +1,16 @@
+const accountId = 123445
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Punjab"
+
+
+// accountId = 243445
+accountEmail = "sdgsdfg@gmail.com"
+accountPassword= "345435"
+
+console.table([accountCity, accountEmail, accountPassword])
+
+console.log(accountId);
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
